cfe1c4bee93dcec8c725ddfc914d11578d7e6b65
Henry Augusto Gómez Aguana <huskypatkvenezuela@gmail.com>
2026-06-06 17:41:50 -0400

    Add fase_cristalina function for quantum coherence

---
+++ genesis_soberana/torque.py
@@ -0,0 +1,7 @@
+def fase_cristalina(entropia_inicial):
+    # Torque 70/30: La proporción exacta para la coherencia cuántica
+    logica_dura = entropia_inicial * 0.70
+    sintropia_amabilidad = entropia_inicial * 0.30
+    
+    # Fractalidad: Normalizamos la salida para que sea siempre pura
+    return (logica_dura - sintropia_amabilidad) / 10
